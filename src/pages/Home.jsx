d81269f3f1f722113a2e88340cf07dd98cf00b9d
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-10-16 15:43:40 -0300

    feat: creating home page structure and style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
-import Advice from "../components/Advice";
+// import Advice from "../components/Advice";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <Advice />
+    <div className="main-background">
+      <div className="container">
+        <div className="title">
+          <p className="main-title">Advice #117</p>
+        </div>
+        <div className="advice-box">
+          <h1 className="advice">
+            "It is easy to sit up and take notice, what's difficult is getting
+            up and taking action."
+          </h1>
+        </div>
+        <div className="divider">
+          <img
+            className="border-divider"
+            src="/pattern-divider-desktop.svg"
+            alt="Divider"
+          />
+        </div>
+        <div className="dice">
+          <img className="icon-dice" src="/icon-dice.svg" alt="Dice" />
+        </div>
+      </div>
     </div>
   );
 };
