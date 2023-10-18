5e773f34c365d7cdde08e0148e62e12e89c003ba
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-10-17 23:50:45 -0300

    feat: using custom hook to make requests and inserting dynamic data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,29 @@
-// import Advice from "../components/Advice";
+import { useState } from "react";
+import useGenerateAdvice from "../hooks/useGenerateAdvice";
 import "../styles/Home.css";
 
 const Home = () => {
+  const { id, data, getAdvice } = useGenerateAdvice();
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const getNextAdvice = () => {
+    setIsButtonDisabled(true);
+    getAdvice();
+
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 2000);
+  };
+
   return (
     <div className="main-background">
       <div className="container">
         <div className="title">
-          <p className="main-title">Advice #117</p>
+          <p className="main-title">{`Advice #${id} `}</p>
         </div>
         <div className="advice-box">
           <h1 className="advice">
-            "It is easy to sit up and take notice, what's difficult is getting
-            up and taking action."
+            <q>{data}</q>
           </h1>
         </div>
         <div className="divider">
@@ -22,7 +34,17 @@ const Home = () => {
           />
         </div>
         <div className="dice">
-          <img className="icon-dice" src="/icon-dice.svg" alt="Dice" />
+          <button
+            onClick={getNextAdvice}
+            disabled={isButtonDisabled}
+            className={`icon-dice ${isButtonDisabled ? "disabled" : "enabled"}`}
+          >
+            <img
+              src="/icon-dice.svg"
+              alt="Dice"
+              className={isButtonDisabled ? "icon-disabled" : "icon-enabled"}
+            />
+          </button>
         </div>
       </div>
     </div>
